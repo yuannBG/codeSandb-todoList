eafb2323e7a16a346c1c2be949ed3c4174a0540f
yuannBG <63868953+yuannBG@users.noreply.github.com>
2021-07-06 20:43:56 +0200

    mise à jour

--- src/App.js
+++ src/App.js
@@ -1,10 +1,40 @@
+import { React, Component } from "react";
 import "./styles.css";
 
-export default function App() {
-  return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Edit to see some magic happen!</h2>
-    </div>
-  );
+class App extends Component {
+  state = {
+    clients: [
+      { id: 1, name: "Od KP" },
+      { id: 2, name: "Al KP" },
+      { id: 3, name: "Roma KP" }
+    ]
+  };
+
+  onDell = (id) => {
+    const clients = this.state.clients.slice();
+    const index = clients.findIndex((client) => client.id === id);
+    clients.splice(index, 1);
+    this.setState({ clients });
+  };
+
+  render() {
+    const client = this.state.clients.map((client) => (
+      <li key={client.id}>
+        {client.name} <button onClick={() => this.onDell(client.id)}>X</button>
+      </li>
+    ));
+    return (
+      <div className="lol">
+        <br />
+        <br />
+        <ul>{client} </ul>
+
+        <br />
+        <br />
+        <input placeholder="Add new client here !" />
+        <button>Add clientc !</button>
+      </div>
+    );
+  }
 }
+export default App;
